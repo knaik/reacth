8cf2efb262ed1fec38fdb3c39d28a6479a505aeb
knaik <kn172@rutgers.edu>
2022-08-04 12:36:44 -0400

    Data fetch with useEffect

--- src/index.js
+++ src/index.js
@@ -3,29 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 function App() {
-  const [name, setName] = useState("Karan");
-  const [admin, setAdmin] = useState(false);
+  const [data, SetData] = useState([]);
 
-  useEffect(() => {
-    /* document.title = `Celebrate ${name}`; */
-    console.log(`Celebrate ${name}`);
-  }, [name]);
-
-  useEffect(() => {
-    console.log(`The user is: ${admin ? "admin" : "not admin"}.`);
-  }, [admin]);
-
-  return (
-    <section>
-      <p> Congratulations {name}!</p>
-      <button onClick={() => setName("Naik")}>Change Winner</button>
-      <p>{admin ? "logged in" : "not logged in"}</p>
-
-      <button onClick={() => setAdmin(!admin)}>Log in</button>
-    </section>
-  );
+  return <p>No users</p>;
 }
-//test
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
