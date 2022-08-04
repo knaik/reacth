ea4f3d3fb532d0ebf3f35c0cb57b1308f16824ff
knaik <kn172@rutgers.edu>
2022-08-04 14:10:41 -0400

    state instead of ref

--- src/index.js
+++ src/index.js
@@ -4,22 +4,35 @@ import "./index.css";
 
 function App() {
   // const [state, dispactch] = useReducer(reducer, initialState);
-  const sound = useRef();
+  const [sound, setSound] = useState("");
+  const [color, setColor] = useState("#000000");
+  /* const sound = useRef();
   const color = useRef();
-
+ */
   const submit = (e) => {
     e.preventDefault();
-    const soundVal = sound.current.value;
-    const colorVal = color.current.value;
-    alert(`${soundVal} sounds like ${colorVal}`);
-    sound.current.value = "";
-    color.current.value = "";
+    /*     const soundVal = sound.current.value;
+    const colorVal = color.current.value; */
+    alert(`${sound} sounds like ${color}`);
+    setSound("");
+    setColor("#000000");
+    /*     sound.current.value = "";
+    color.current.value = ""; */
   };
 
   return (
     <form onSubmit={submit}>
-      <input ref={sound} type="text" placeholder="sound..." />
-      <input ref={color} type="color" />
+      <input
+        value={sound}
+        type="text"
+        placeholder="sound..."
+        onChange={(e) => setSound(e.target.value)}
+      />
+      <input
+        value={color}
+        type="color"
+        onChange={(e) => setColor(e.target.value)}
+      />
       <button>ADD</button>
     </form>
   );
