92a420b7df87749b96ea90720a53402433bb7e4e
knaik <kn172@rutgers.edu>
2022-08-04 12:49:19 -0400

    dependency array so you don't take down api

--- src/index.js
+++ src/index.js
@@ -5,6 +5,25 @@ import "./index.css";
 function App() {
   const [data, SetData] = useState([]);
 
+  useEffect(() => {
+    fetch(`https://api.github.com/users`)
+      .then((response) => response.json())
+      .then(SetData);
+  }, []);
+
+  if (data) {
+    return (
+      <div>
+        <ul>
+          {data.map((user) => (
+            <li key={user.id}>{user.login}</li>
+          ))}
+        </ul>
+        <button onClick={() => SetData([])}>Remove Data</button>
+      </div>
+    );
+  }
+
   return <p>No users</p>;
 }
 
