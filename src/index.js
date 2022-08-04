1c72f3b22c5d1f9d4ad08a0e6235429b32fc2de1
knaik <kn172@rutgers.edu>
2022-08-04 13:47:29 -0400

    reacthook reducer

--- src/index.js
+++ src/index.js
@@ -2,12 +2,31 @@ import React, { useState, useEffect, useReducer } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const initialState = {
+  message: "hi",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "yell":
+      return {
+        message: "Hey!",
+      };
+    case "whisper":
+      return {
+        message: "hi there",
+      };
+  }
+}
+
 function App() {
-  const [number, setNumber] = useReducer(
-    (number, newNumber) => number + newNumber,
-    0
+  const [state, dispactch] = useReducer(reducer, initialState);
+
+  return (
+    <>
+      <p> Message {state.message} </p>
+    </>
   );
-  return <h1 onClick={() => setNumber(1)}>{number}</h1>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
