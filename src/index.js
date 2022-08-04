97e4a21084fd79105dfd3ee4e0c978ed513d7945
knaik <kn172@rutgers.edu>
2022-08-04 13:29:42 -0400

    useReducer

--- src/index.js
+++ src/index.js
@@ -1,30 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
 function App() {
-  const [data, SetData] = useState([]);
-
-  useEffect(() => {
-    fetch(`https://api.github.com/users`)
-      .then((response) => response.json())
-      .then(SetData);
-  }, []);
-
-  if (data) {
-    return (
-      <div>
-        <ul>
-          {data.map((user) => (
-            <li key={user.id}>{user.login}</li>
-          ))}
-        </ul>
-        <button onClick={() => SetData([])}>Remove Data</button>
-      </div>
-    );
-  }
-
-  return <p>No users</p>;
+  const [number, setNumber] = useReducer(
+    (number, newNumber) => number + newNumber,
+    0
+  );
+  return <h1 onClick={() => setNumber(1)}>{number}</h1>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
